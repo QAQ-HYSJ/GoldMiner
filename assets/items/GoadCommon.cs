b900e7bb80b44cb75b8e07dd62b5129c4ea97c60
hy <441717627@qq.com>
2023-09-26 14:59:00 +0800

    改项目名，清缓存，并加入了石头

---
+++ assets/items/GoadCommon.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class GoadCommon : Item
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		base._Ready();
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
+	}
+}
