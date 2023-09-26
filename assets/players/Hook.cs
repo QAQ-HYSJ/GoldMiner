16cd656392d1fb60f703a9b5a7c74bd9253b2e8e
hy <441717627@qq.com>
2023-09-27 00:56:47 +0800

    完善人物动画和音效

--- assets/players/Hook.cs
+++ assets/players/Hook.cs
@@ -7,15 +7,20 @@ public partial class Hook : Marker2D
 	private bool _goback = true;
 	private Vector2 _direction;
 	private bool _hooking;
-	public void GoHook()
+	public void GoHook()  // 出钩
 	{
 		if (!_hooking)
 		{
 			_direction = new Vector2((float)-Math.Sin(Rotation), (float)Math.Cos(Rotation)).Normalized();
 			_hooking = true;
 			_goback = true;
+
+			GetParent<Player1>().Pause();
+			GetParent<Player1>().Frame = 2;
 			GetNode<AnimationPlayer>("AnimationPlayer").Pause();
 			GetNode<Timer>("Timer").Start();
+
+			GetNode<AudioStreamPlayer>("GrabStart").Play();
 		}
 	}
 
@@ -24,17 +29,19 @@ public partial class Hook : Marker2D
 		GetNode<Timer>("Timer").Timeout += OnBack;
 		GetNode<Area2D>("HitBox").AreaEntered += (@object) => OnAreaEntered(@object);
 		Global.hook = this;
+		GetParent<Player1>().Pause();
+		GetParent<Player1>().Frame = 0;
 	}
-	public override void _Process(double delta)
+	public override async void _Process(double delta)
 	{
 		QueueRedraw();
 		if (_hooking)
 		{
-			if(_goback)  // 出钩
+			if (_goback)  // 出钩
 			{
 				Position += (float)delta * _direction * 200f;
 			}
-			else   		 // 回钩
+			else         // 回钩
 			{
 				Position += -(float)delta * _direction * (200f - Weight);
 			}
@@ -42,21 +49,34 @@ public partial class Hook : Marker2D
 			if (Position.Y <= 10)
 			{
 				Position = new Vector2(-7, 10);      // 勾子原点
-				GetNode<AnimationPlayer>("AnimationPlayer").Play();
 				GetNode<Event>("/root/Event").EmitSignal(Event.SignalName.BackHook);
+				GetNode<AudioStreamPlayer>("GrabBack").Stop();
+				GetParent<Player1>().Pause();
+				GetParent<Player1>().Frame = 0;
 				_hooking = false;
 				Weight = 0;
+
+				//await GetTree().CreateTimer(1.0).ToSignal("Timeout");
+				GetNode<AnimationPlayer>("AnimationPlayer").Play();
 			}
 		}
 	}
 	private void OnAreaEntered(Area2D @object)
 	{
 		_goback = false;
+		GetParent<Player1>().Play();
+		GetNode<AudioStreamPlayer>("GrabBack").Play();
 	}
 
 	private void OnBack()
 	{
+		if (_goback)
+		{
+			GetParent<Player1>().Play();
+			GetNode<AudioStreamPlayer>("GrabBack").Play();
+		}
 		_goback = false;
+
 	}
 
 	public void Reset()
