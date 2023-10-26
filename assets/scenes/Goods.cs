58ba2d9ab42b21bf94634da4a1762599c042fcc1
hy <441717627@qq.com>
2023-10-26 18:24:51 +0800

    完成商店界面

--- assets/scenes/Goods.cs
+++ assets/scenes/Goods.cs
@@ -3,15 +3,38 @@ using System;
 
 public partial class Goods : Control
 {
-    public override void _Ready()
+    private int[] prices = new int[5];
+    private string[] describes = new string[5]
     {
+        "雷管：获得一个雷管",
+        "力量加强剂：下一关加快钩子速度",
+        "幸运草：下一关更高概率袋子中出现高价值物品",
+        "宝石抛光剂：下一关钻石能卖出更高价值",
+        "石头收藏书：下一关石头能卖出更高价值"
+    };
+    public override void _Ready()  // 随机出现物品
+    {
+        bool temp = true;  //保底得有一个
         for (int i = 0; i < 5; i++)
         {
             if (GD.RandRange(1, 2) == 1)
             {
                 GetChild<TextureRect>(i).Visible = false;
             }
+            else
+            {
+                prices[i] = GD.RandRange(2, 999);
+                GetChild<TextureRect>(i).GetNode<Label>("Label").Text = "$" + prices[i];
+                temp = false;  // 一旦有了物品，则为false，后面无需再管
+            }
         }
+        if (temp)
+        {
+            int num = GD.RandRange(0, 5);
+            GetChild<TextureRect>(num).Visible = true;
+            prices[num] = GD.RandRange(1, 999);
+        }
+        GetParent().GetNode<Label>("Label").Text = "";
     }
     public override void _Input(InputEvent @event)
     {
@@ -21,7 +44,7 @@ public partial class Goods : Control
             {
                 if (GetChild<TextureRect>(i).GetRect().HasPoint(inputEventMouseButton.Position) && GetChild<TextureRect>(i).Visible)  // 且可见
                 {
-                    GD.Print("点击了第", i, "件商品");
+                    GetChild<TextureRect>(i).Visible = false;
                 }
             }
         }
@@ -30,15 +53,16 @@ public partial class Goods : Control
     {
         if (GetChild<TextureRect>(index).Visible)  // 且可见
         {
-            GD.Print("选中了第" + index + "件商品");
-            GetChild<TextureRect>(index).Modulate = Colors.Gray;
+            GetChild<TextureRect>(index).SelfModulate = Colors.Gray;
+            GetParent().GetNode<Label>("Label").Text = describes[index];
         }
     }
     private void On_TextureRect_MouseExited()
     {
         for (int i = 0; i < 5; i++)
         {
-            GetChild<TextureRect>(i).Modulate = Colors.White;
+            GetChild<TextureRect>(i).SelfModulate = Colors.White;
         }
+        GetParent().GetNode<Label>("Label").Text = "";
     }
 }
