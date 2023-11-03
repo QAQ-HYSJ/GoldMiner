69fa767b3b8e8c2bcda9753847e2147254d628e9
hy <441717627@qq.com>
2023-11-03 22:46:21 +0800

    修复重新开始时，数值继承的bug

--- assets/scenes/End.cs
+++ assets/scenes/End.cs
@@ -3,12 +3,14 @@ using System;
 
 public partial class End : TextureRect
 {
-	public override void _Ready()
+	public override async void _Ready()
 	{
 		if (Data.Singleton.Money < Data.Singleton.goal)
 		{
 			GetNode<Label>("Panel/Label").Text = "您未达成目标\n得分：" + Data.Singleton.Money;
 			GetNode<Label>("Panel/Label").Set("theme_override_font_sizes/font_size", 20);
+			await ToSignal(GetTree().CreateTimer(1), SceneTreeTimer.SignalName.Timeout);
+			GetNode<Button>("Panel/Button").Show();
 		}
 		else
 		{
@@ -17,9 +19,10 @@ public partial class End : TextureRect
 	}
 	private void On_AudioStreamPlayer_Finished()
 	{
-		if (Data.Singleton.Money < Data.Singleton.goal)
-			GetTree().ChangeSceneToFile("res://assets/scenes/FastStart.tscn");
-		else
-			GetTree().ChangeSceneToFile("res://assets/scenes/Shop.tscn");
+		GetTree().ChangeSceneToFile("res://assets/scenes/Shop.tscn");
+	}
+	private void On_Button_Pressed()
+	{
+		GetTree().ChangeSceneToFile("res://assets/scenes/FastStart.tscn");
 	}
 }
--- assets/scenes/FastStart.cs
+++ assets/scenes/FastStart.cs
@@ -3,7 +3,18 @@ using System;
 
 public partial class FastStart : TextureRect
 {
-	private void On_Single_Pressed()
+    public override void _Ready()
+    {
+        Data.Singleton.LevelNum = 1;
+		Data.Singleton.Money = 0;
+		Data.Singleton.HasBuyStrengthBuff = false;
+		Data.Singleton.RockBuff = false;
+		Data.Singleton.LuckyBuff = false;
+		Data.Singleton.GemPolishBuff = false;
+		Data.Singleton.TempPlayer1DynamiteNum = 0;
+		Data.Singleton.TempPlayer2DynamiteNum = 0;
+    }
+    private void On_Single_Pressed()
 	{
 		Data.Singleton.gameMode = false;
 		GetTree().ChangeSceneToFile("res://assets/scenes/Begin.tscn");
